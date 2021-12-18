e7d1e0a68e797e053f091ca01ec2966b9c5072df
YongHeon Choi <tiger9966@naver.com>
2021-12-18 14:56:27 +0900

    add backup structure

---
+++ app/src/main/java/com/example/liststopwatchjava/MainSettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.liststopwatchjava;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+
+public class MainSettingActivity extends WearableActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_setting);
+
+    }
+}
